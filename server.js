c170b905fd837decd9b02fb2f5fb7631f67b7a6d
htunyarzar80 <htunyarzar2001@gmail.com>
2024-08-02 11:53:33 +0630

    [Complete] setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+
+app.use("/",(req,res)=>{
+    res.send('hello world!')
+})
+
+//port 
+const port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
